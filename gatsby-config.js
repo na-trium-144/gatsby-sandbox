b3bcf9edf0b1fe913fefbd2b427321e02c908a63
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2023-11-27 00:27:01 +0900

    part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,5 @@ module.exports = {
     title: `gatsby-sandbox`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
-}
+  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 // Step 1: Import React
 import * as React from "react";
 import { Link } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
 import Layout from "../components/layout";
 
 // Step 2: Define your component
@@ -8,6 +9,10 @@ const IndexPage = () => {
   return (
     <Layout pageTitle="Home">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+      />
     </Layout>
   );
 };
